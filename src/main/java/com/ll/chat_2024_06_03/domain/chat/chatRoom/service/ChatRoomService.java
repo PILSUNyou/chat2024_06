ac4f16b72fc95b7db38302ed340bc783e7a31cc5
PILSUNyou <dbyo1125@naver.com>
2024-06-04 16:46:24 +0900

    NotProd 생성

--- src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/service/ChatRoomService.java
+++ src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/service/ChatRoomService.java
@@ -2,26 +2,42 @@ package com.ll.chat_2024_06_03.domain.chat.chatRoom.service;
 
 import com.ll.chat_2024_06_03.domain.chat.chatRoom.entity.ChatRoom;
 import com.ll.chat_2024_06_03.domain.chat.chatRoom.repository.ChatRoomRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
 
-    public void make(String name){
+    @Transactional
+    public ChatRoom make(String name){
         ChatRoom chatRoom = ChatRoom.builder()  // application에서 EnableJpaAuditing 에너테이션을 사용해서 createdate를 쓰지 않아도 된다.
                 .name(name)
                 .build();
 
         chatRoomRepository.save(chatRoom);
+
+        return chatRoom;
     }
 
 
     public List<ChatRoom> findAll() {
         return chatRoomRepository.findAll();
+    }
+
+    public Optional<ChatRoom> findById(long roomId) {
+        return chatRoomRepository.findById(roomId);
+    }
+
+    @Transactional
+    public void write(long roomId, String writerName, String content) {
+        ChatRoom chatRoom = chatRoomRepository.findById(roomId).get();
+
+        chatRoom.writeMessage(writerName, content);
     }
 }
